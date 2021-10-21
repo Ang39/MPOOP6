59305c1d3a3307839734f7bb1d6fd3ced68d01d2
Anduaga-Balestr <>
2021-10-21 19:12:03 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+/**
+*@author Angel Anduaga
+*/
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("La suma es: "+suma(2,2));
+  }
+/**
+*@param a Primer entero de la ecuación 
+*@param b Segundo entero de la ecuación
+*@return la suma de los enteros a más b
+*/
+  public static int suma(int a, int b){
+    return a+b;
+  }
+}
